2b24e9bd9aa6c9ebe93823f340cb4a6c6da23a8d
Supriya Singla <supriya.singla@certus.plus>
2019-05-17 14:09:32 +0530

    getWeather implementation

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const apiKey = '134495aa72cbcd0df97f80cceab4fc1c';
 
 const getWeather = async locations => {
 
+    try{
+        if(locations instanceof Array){
+            //if input is an array
+            const fetchUrls = locations.map(location=>{
+                //for cities
+                if(isNaN(location)) return(r2(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`).json);
+                //for zipcodes
+                else return(r2(`http://api.openweathermap.org/data/2.5/weather?zip=${location}&units=imperial&appid=${apiKey}`).json);
+            })
+            const weatherDetails = await Promise.all(fetchUrls);
+            console.log(weatherDetails)
+            return weatherDetails;
+            }
+            else{
+                //return error if input is not an array
+                return({ cod: '400' , message: 'input should be a list'});
+            }
+    }
+    catch(e){
+
+        return({ cod: '500' , message: e.message});
+
+    }
+               
 }
 
 module.exports={
